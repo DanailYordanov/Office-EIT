0ab2cb1f2670b22977e37bef3512931d5391bdc6
Danail Yordanov <danail.yordanov1@gmail.com>
2021-11-28 22:35:18 +0200

    Extend base user model

--- project/users/models.py
+++ project/users/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    id_card_expiration = models.DateField(null=True)
+    drivers_license_expiration = models.DateField(null=True)
+    professional_competence = models.DateField(null=True)
+    adr_expiration = models.DateField(null=True)
+    digital_card_expiration = models.DateField(null=True)
+    psychological_test_expiration = models.DateField(null=True)
+    pasport_expiration = models.DateField(null=True)
