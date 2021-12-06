dc137e53184e2abb8a6fc49b4c8f60c9bbddc62c
Danny <danail.yordanov1@gmail.com>
2021-12-06 16:27:49 +0200

    Add small changes to the profile details

--- project/users/views.py
+++ project/users/views.py
@@ -10,11 +10,13 @@ def profile_details(request, pk=None):
         if request.user.is_staff:
             user = get_object_or_404(get_user_model(), pk=pk)
             redirect_url = reverse('profile-details', kwargs={'pk': pk})
+            message = f'Редактиране на информацията на {user}'
         else:
             raise PermissionDenied
     else:
         user = request.user
         redirect_url = reverse('profile-details')
+        message = f'Редактиране на вашата информация'
 
     if request.method == 'POST':
         form = ProfileDetailsForm(request.POST, instance=user)
@@ -27,6 +29,7 @@ def profile_details(request, pk=None):
 
     context = {
         'form': form,
+        'message': message,
         'redirect_url': redirect_url
     }
 
